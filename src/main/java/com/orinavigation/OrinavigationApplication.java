39eab6a0d633ddb59a4ef8b349d9a71389cca3b1
cuggiser <zhiyongchoucug@gmail.com>
2017-07-30 17:24:20 +0800

    initial

---
+++ src/main/java/com/orinavigation/OrinavigationApplication.java
@@ -0,0 +1,12 @@
+package com.orinavigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrinavigationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrinavigationApplication.class, args);
+	}
+}
